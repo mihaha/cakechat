3ddce81022fb061dee56e47bae933e356a6522c5
Khalman Mikhail <khalman.m@gmail.com>
2018-08-25 20:17:21 +0300

    add memory usage

--- cakechat/dialog_model/inference/candidates/sampling.py
+++ cakechat/dialog_model/inference/candidates/sampling.py
@@ -1,10 +1,13 @@
+import resource
+
 import numpy as np
-from six.moves import xrange
 import theano
+from six.moves import xrange
 
 from cakechat.dialog_model.inference.candidates.abstract_generator import AbstractCandidatesGenerator
 from cakechat.dialog_model.inference.service_tokens import ServiceTokensIDs
 from cakechat.dialog_model.inference.utils import get_next_token_prob_one_step, get_thought_vectors
+from cakechat.utils.logger import get_logger
 
 
 class TokenSampler(object):
@@ -22,12 +25,16 @@ class TokenSampler(object):
         self._used_tokens_ids = [[] for _ in xrange(batch_size)]
         self._repetition_penalization_coefficient = repetition_penalization_coefficient
 
-    def sample(self, probabilities, sample_idx, temperature=1.0):
+    def sample(self, probabilities, sample_idx, temperature, mem_use_callback):
         """
         Sample using individual priors for each sample_idx.
         Also updates individual priors for each sample in batch.
         We need individual priors to prevent the model from repeating the same tokens over and over again in one
         response.
+
+        THIS FUNCTION MAY CHANGE THE PROBABILITIES AS A SIDE-EFFECT.
+        PLEASE BE CAREFUL.
+        RUN sampler.sample(probabilities.copy(), sample_idx) if you want to reuse the probabilities afterwards
 
         probabilities: Probabilities of each token. The distribution given by these probabilities
             is used by this function to sample the token.
@@ -35,45 +42,67 @@ class TokenSampler(object):
         temperature: Temperature for sampling. Temperature has to be a positive number.
         :return: Index of sampled token
         """
+        if not isinstance(probabilities, np.ndarray):
+            probabilities = np.array(probabilities)
+
+        # mem_use_callback('sampler.sample: fn is called')
         # To make the repetition penalization invariant to the original temperature we have to adjust the coefficient:
         repetition_penalize_coefficient = np.exp(np.log(self._repetition_penalization_coefficient) / temperature)
-        # Back-up the array to avoid side-effects (otherwise the function will change the probabilities passed as an
-        # argument)
-        probabilities = np.copy(probabilities)
+        # mem_use_callback('sampler.sample: exp(log(x / t))')
 
         probabilities[self._banned_tokens_ids] = 0
+        # mem_use_callback('sampler.sample: ban banned tokens')
         probabilities[self._used_tokens_ids[sample_idx]] /= repetition_penalize_coefficient
+        # mem_use_callback('sampler.sample: pen used tokens')
 
         probabilities /= np.sum(probabilities)
+        mem_use_callback('sampler.sample: normalize')
         token_id = np.random.choice(probabilities.shape[0], replace=False, p=probabilities)
+        mem_use_callback('sampler.sample: random choice')
 
         # Update used tokens list
         if token_id not in self._non_penalizable_tokens_ids:
             self._used_tokens_ids[sample_idx].append(token_id)
+        # mem_use_callback('sampler.sample: update used tokens')
 
         return token_id
 
 
 class SamplingCandidatesGenerator(AbstractCandidatesGenerator):
     def __init__(self, nn_model, temperature, samples_num, repetition_penalization_coefficient):
+        self._logger = get_logger(self.__class__.__module__ + '.' + self.__class__.__name__)
+
         self._nn_model = nn_model
         self._temperature = temperature
         self._samples_num = samples_num
         self._service_tokens_ids = ServiceTokensIDs(nn_model.token_to_index)
         self._repetition_penalization_coefficient = repetition_penalization_coefficient
 
+        self._mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+    def _update_memory_usage(self, log_str):
+        cur_mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        memory_usage_increased = cur_mem_usage > self._mem_usage
+
+        if memory_usage_increased:
+            self._logger.warning('[{}] Memory usage increased after: {}kb->{}kb (+{}kb)'.format(
+                log_str, self._mem_usage, cur_mem_usage, cur_mem_usage - self._mem_usage))
+            self._mem_usage = cur_mem_usage
+
     def _sample_response(self, thought_vectors, condition_ids, output_seq_len):
         batch_size = thought_vectors.shape[0]
         sampler = TokenSampler(batch_size, self._service_tokens_ids.banned_tokens_ids,
                                self._service_tokens_ids.non_penalizable_tokens_ids,
                                self._repetition_penalization_coefficient)
+
+        # self._update_memory_usage('TokenSampler initialized')
+
         # For each candidate in the batch, for each layer of the decoder we need hidden_states_dim numbers to store
         # this array
         hidden_states_batch = np.zeros(
             (batch_size, self._nn_model.decoder_depth, self._nn_model.hidden_layer_dim),
             dtype=theano.config.floatX)  # By default, numpy has dtype=np.float64, but this array is passed
         # right into theano functions, so we need to have explicit type declaring here.
-
         response_tokens_ids = np.full(
             (batch_size, output_seq_len), self._service_tokens_ids.pad_token_id, dtype=np.int32)
 
@@ -82,18 +111,26 @@ class SamplingCandidatesGenerator(AbstractCandidatesGenerator):
 
         # Fill in first tokens of each response in the batch:
         response_tokens_ids[:, 0] = self._service_tokens_ids.start_token_id
+
         for token_idx in xrange(1, output_seq_len):  # Starting with the second token
+            # self._update_memory_usage('Before get_next_token_prob_one_step')
+
             hidden_states_batch, next_token_probs_batch = \
                 get_next_token_prob_one_step(self._nn_model, thought_vectors, hidden_states_batch,
                                              response_tokens_ids[:, token_idx - 1],  # previous token for each response
                                              condition_ids,
                                              temperature=self._temperature)
+            # self._update_memory_usage('After get_next_token_prob_one_step')
 
             for response_idx, next_token_probs in enumerate(next_token_probs_batch):
                 if is_response_finished[response_idx]:
                     continue
+                # self._update_memory_usage('Before sampler.sample()')
 
-                next_token_id = sampler.sample(next_token_probs, response_idx, self._temperature)
+                next_token_id = sampler.sample(next_token_probs, response_idx, self._temperature,
+                                               self._update_memory_usage)
+                # self._update_memory_usage('After sampler.sample()', use_guppy=True)
+
                 response_tokens_ids[response_idx, token_idx] = next_token_id
 
                 if next_token_id in [self._service_tokens_ids.eos_token_id, self._service_tokens_ids.pad_token_id]:
@@ -115,9 +152,15 @@ class SamplingCandidatesGenerator(AbstractCandidatesGenerator):
         All the rest of the sequence is filled with PAD_TOKENs.
         """
         thought_vectors = get_thought_vectors(self._nn_model, context_tokens_ids)
+        self._logger.info(
+            '[Sampling candidates] Memory usage: {}kb'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
+
         sampled_candidates = [
             self._sample_response(thought_vectors, condition_ids, output_seq_len) for _ in xrange(self._samples_num)
         ]
+        self._logger.info(
+            '[Finished sampling candidates] Memory usage: {}kb'.format(
+                resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
 
         # Transpose the result: candidate_id x batch_size x seq_len -> batch_size x candidate_id x seq_len
         return np.swapaxes(sampled_candidates, 0, 1)
